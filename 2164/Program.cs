2bf232aeb417e5317c6f69c4f11709745332a497
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-07 23:01:59 +0900

    add

---
+++ 2164/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VS_CODE{
+    class Program{
+        static void Main(string[] args){
+            string temp;
+            string[] aeiou = {"a", "e", "i", "o", "u"};
+            while(!(temp = Console.ReadLine()).Equals("#")){
+                int count = 0;
+                for(int i=0; i<temp.Length; i++){
+                    for(int j=0; j<aeiou.Length; j++){
+                        if(temp.Substring(i, 1).ToLower().Equals(aeiou[j])) count++;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+
+        }
+    }
+}
