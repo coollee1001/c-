82bdd482c681c927d314ea0c6a5cb432a34d770a
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-07 23:11:24 +0900

    add

---
+++ 2083/Program.cs
@@ -0,0 +1,9 @@
+﻿string temp;
+while(!(temp = Console.ReadLine()).Equals("# 0 0")){
+    string[] info = temp.Split(" ");
+    temp = "Junior";
+    if(17 < int.Parse(info[1]) || 80 <= int.Parse(info[2])){
+        temp = "Senior";
+    }
+    Console.WriteLine("{0} {1}", info[0], temp);
+}
