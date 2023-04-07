f40453dcadee86aa45bae848a6068215f33a26b9
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-07 23:16:21 +0900

    add

---
+++ 2440/Program.cs
@@ -0,0 +1,9 @@
+﻿//https://www.acmicpc.net/problem/2440
+
+int temp = int.Parse(Console.ReadLine());
+for(int i=temp; i>0; i--){
+    for(int j=i; j>0; j--){
+        Console.Write("*");
+    }
+    Console.Write("\n");
+}
