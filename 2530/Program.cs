dc8be079f698142481f5098904516ce372a95fcb
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-08 07:58:36 +0900

    add

---
+++ 2530/Program.cs
@@ -0,0 +1,12 @@
+﻿//https://www.acmicpc.net/problem/2530
+
+string[] input_currentTime = Console.ReadLine().Split(" ");
+int input_cookTime = int.Parse(Console.ReadLine());
+
+DateTime currentTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+currentTime = currentTime.AddHours(int.Parse(input_currentTime[0]));
+currentTime = currentTime.AddMinutes(int.Parse(input_currentTime[1]));
+currentTime = currentTime.AddSeconds(int.Parse(input_currentTime[2]));
+currentTime = currentTime.AddSeconds(input_cookTime);
+
+Console.WriteLine("{0} {1} {2}", currentTime.Hour, currentTime.Minute, currentTime.Second);
