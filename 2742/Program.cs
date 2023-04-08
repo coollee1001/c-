3541759fcb3692b6961debcb7c24d4afcf96ed55
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-09 00:05:11 +0900

    29

---
+++ 2742/Program.cs
@@ -0,0 +1,11 @@
+﻿//https://www.acmicpc.net/problem/2742
+
+using System.Text;
+
+int input_N = int.Parse(Console.ReadLine());
+StringBuilder answer = new StringBuilder();
+
+for(int i=input_N; i>0; i--){
+    answer.AppendLine(i.ToString());
+}
+Console.WriteLine(answer);
---
+++ 2752/Program.cs
@@ -0,0 +1,15 @@
+﻿//https://www.acmicpc.net/problem/2752
+
+string[] input_threeInt = Console.ReadLine().Split(" ");
+int[] input_sort = new int[input_threeInt.Length];
+
+for(int i=0; i<input_threeInt.Length; i++){
+    input_sort[i] = int.Parse(input_threeInt[i]);
+}
+
+Array.Sort(input_sort);
+
+for(int i=0; i<input_sort.Length; i++){
+    Console.Write(input_sort[i]);
+    if(i != input_sort.Length-1) Console.Write(" ");
+}
