332dd79fc17813fc9e8d48b7de2aae90d5a8b10e
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-10 00:03:14 +0900

    32

---
+++ bronze4/4589.Program.cs
@@ -0,0 +1,34 @@
+﻿//https://www.acmicpc.net/problem/4589
+
+using System.Text;
+
+int n, sbLength;
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+sw.AutoFlush = true;
+
+StringBuilder sb = new StringBuilder();
+n = int.Parse(sr.ReadLine());
+for(int i=1; i<=n; i++){
+    string[] temp = sr.ReadLine().Split(" ");
+    string[] sortTemp = (string[])temp.Clone();
+    string[] reverseTemp = (string[])temp.Clone();
+    Array.Sort(sortTemp);
+    Array.Sort(reverseTemp); Array.Reverse(reverseTemp);
+    for(int j=0; j<temp.Length; j++){
+        if(!temp[j].Equals(sortTemp[j])) {
+            if(!temp[j].Equals(reverseTemp[j])){
+                sb.AppendLine("Unordered");
+                break;
+            }
+        }
+    }
+    sbLength = sb.ToString().Split("\n").Length -1;
+    if(sbLength != i) sb.AppendLine("Ordered");
+}
+Console.WriteLine("Gnomes:");
+sw.Write(sb.ToString());
+
+// github용 파일 복사
+Filecopy fc = new Filecopy(4471);
+File.Copy(fc.getThisPath(), fc.getTargetPath());
---
+++ bronze4/4696.Program.cs
@@ -0,0 +1,24 @@
+﻿//https://www.acmicpc.net/problem/4696
+
+using System.Text;
+
+float n;
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+sw.AutoFlush = true;
+
+StringBuilder sb = new StringBuilder();
+while((n = float.Parse(sr.ReadLine())) != 0){
+    float sum = (1) // 나
+        + (n) // 아내
+        + (n * n) // 자루
+        + (n * n * n) //고양이
+        + (n * n * n * n) //새끼 고양이
+        ;
+    sb.AppendLine(String.Format("{0:0.00}", Math.Round(sum * 100)/100.0));
+}
+sw.WriteLine(sb.ToString());
+
+// github용 파일 복사
+Filecopy fc = new Filecopy(4696);
+File.Copy(fc.getThisPath(), fc.getTargetPath());
