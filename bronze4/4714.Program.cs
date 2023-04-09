75fb191aa1bf3b8c490f651d4130b031b0a78fb7
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-10 04:57:52 +0900

    33

---
+++ bronze4/4714.Program.cs
@@ -0,0 +1,19 @@
+﻿//https://www.acmicpc.net/problem/4714
+
+using System.Text;
+
+float n;
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+sw.AutoFlush = true;
+
+StringBuilder sb = new StringBuilder();
+while((n = float.Parse(sr.ReadLine())) != -1.0){
+    float moonWeigh = (float)(n * 0.167);
+    sb.AppendLine(String.Format("Objects weighing {0:0.00} on Earth will weigh {1:0.00} on the moon.", n, moonWeigh));
+}
+sw.WriteLine(sb.ToString());
+
+// github용 파일 복사
+Filecopy fc = new Filecopy(4714);
+File.Copy(fc.getThisPath(), fc.getTargetPath());
---
+++ bronze4/5300.Program.cs
@@ -0,0 +1,23 @@
+﻿//https://www.acmicpc.net/problem/5300
+
+using System.Text;
+
+int n;
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+sw.AutoFlush = true;
+
+StringBuilder sb = new StringBuilder();
+n = int.Parse(sr.ReadLine());
+for(int i=1; i<=n; i++){
+    sb.Append(i + " ");
+    if(i%6 == 0 || i==n){
+        sb.Append("Go!");
+        if(i!=n) sb.Append(" ");
+    }
+}
+sw.WriteLine(sb.ToString());
+
+// github용 파일 복사
+Filecopy fc = new Filecopy(5300);
+File.Copy(fc.getThisPath(), fc.getTargetPath());
