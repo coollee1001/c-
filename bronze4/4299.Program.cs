5d1164f8054b230f8367b50e39d6ba829c4dd7e8
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-09 14:22:56 +0900

    31

---
+++ bronze4/4299.Program.cs
@@ -0,0 +1,23 @@
+﻿//https://www.acmicpc.net/problem/4299
+
+using System.Text;
+
+string[] temp;
+int[] int_temp;
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+sw.AutoFlush = true;
+
+temp = sr.ReadLine().Split(" ");
+int_temp = new int[temp.Length];
+
+for(int i=0; i<temp.Length; i++){
+    int_temp[i] = int.Parse(temp[i]);
+}
+
+//계산
+int x = (int_temp[0] + int_temp[1])/2;
+int y = (int_temp[1] - x)*-1;
+if((x+y) == int_temp[0] && (x-y) == int_temp[1] && 0 <= x && 0 <= y)
+sw.WriteLine("{0} {1}", (x < y)?y:x, (x<y)?x:y);
+else sw.WriteLine(-1);
---
+++ bronze4/4470.Program.cs
@@ -0,0 +1,15 @@
+﻿//https://www.acmicpc.net/problem/4470
+
+using System.Text;
+
+int n, count = 1;
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+sw.AutoFlush = true;
+
+StringBuilder sb = new StringBuilder();
+n = int.Parse(sr.ReadLine());
+for(int i=0; i<n; i++){
+    sb.AppendLine(count++ + ". " + sr.ReadLine());
+}
+sw.Write(sb.ToString());
