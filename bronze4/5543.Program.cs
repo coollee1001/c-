a00be4cf4aa191aeaa8c6b7f36daa0df851f19a9
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-10 20:41:51 +0900

    36

---
+++ bronze4/5543.Program.cs
@@ -0,0 +1,26 @@
+﻿//https://www.acmicpc.net/problem/5543
+
+using System.Text;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StringBuilder sb = new StringBuilder();    
+sw.AutoFlush = true;
+
+int n = 3;
+int minBugger = 2000, minDrink = 2000;
+for(int i=0; i<n; i++){
+    int tempBugger = int.Parse(sr.ReadLine());
+    if(tempBugger < minBugger) minBugger = tempBugger;
+}
+n = 2;
+for(int i=0; i<n; i++){
+    int tempDrink = int.Parse(sr.ReadLine());
+    if(tempDrink < minDrink) minDrink = tempDrink;
+}
+Console.Write(minBugger + minDrink - 50);
+
+
+// github용 파일 복사
+Filecopy fc = new Filecopy(5543);
+File.Copy(fc.getThisPath(), fc.getTargetPath());
---
+++ bronze4/5554.Program.cs
@@ -0,0 +1,28 @@
+﻿//https://www.acmicpc.net/problem/5554
+
+using System.Text;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StringBuilder sb = new StringBuilder();    
+sw.AutoFlush = true;
+
+int n = 4, sum = 0;
+for(int i=0; i<n; i++){
+    sum += int.Parse(sr.ReadLine());
+}
+sw.WriteLine(sum/60);
+sw.WriteLine(sum%60);
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
