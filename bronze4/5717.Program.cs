c7475894912c360cbf6df4246a609eccd33eaee9
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-12 06:58:02 +0900

    38

---
+++ bronze4/5717.Program.cs
@@ -0,0 +1,28 @@
+﻿//https://www.acmicpc.net/problem/5717
+
+using System.Text;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StringBuilder sb = new StringBuilder();    
+sw.AutoFlush = true;
+
+string inputData;
+while((inputData = sr.ReadLine()) != "0 0"){
+    string[] inputDataSplit = inputData.Split(" ");
+    sb.AppendLine(String.Format("{0}", int.Parse(inputDataSplit[0]) + int.Parse(inputDataSplit[1])));
+}
+sw.WriteLine(sb.ToString());
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
---
+++ bronze4/5928.Program.cs
@@ -0,0 +1,33 @@
+﻿//https://www.acmicpc.net/problem/5928
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+string[] inputDate = sr.ReadLine().Split(" ");
+int[] inputDateParseInt = new int[3];
+int sum = 0, tempsum = 0;
+for(int i=0; i<inputDateParseInt.Length; i++){
+    inputDateParseInt[i] = int.Parse(inputDate[i]);
+}
+sum += inputDateParseInt[0] * 24 * 60;
+sum += inputDateParseInt[1] * 60;
+sum += inputDateParseInt[2];
+
+tempsum += 11 * 24 * 60;
+tempsum += 11 * 60;
+tempsum += 11;
+
+if(sum < tempsum) Console.WriteLine(-1);
+else Console.WriteLine(sum - tempsum);
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
