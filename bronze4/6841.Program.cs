fd0963208589577aa75f82fa478df89eb45173df
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-12 11:42:46 +0900

    41

---
+++ bronze4/6841.Program.cs
@@ -0,0 +1,45 @@
+﻿//https://www.acmicpc.net/problem/6841
+
+using System.Collections;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+Hashtable hash = new Hashtable();
+hash.Add("CU", "see you");
+hash.Add(":-)", "I’m happy");
+hash.Add(":-(", "I’m unhappy");
+hash.Add(";-)", "wink");
+hash.Add(":-P", "stick out my tongue");
+hash.Add("(~.~)", "sleepy");
+hash.Add("TA", "totally awesome");
+hash.Add("CCC", "Canadian Computing Competition");
+hash.Add("CUZ", "because");
+hash.Add("TY", "thank-you");
+hash.Add("YW", "you’re welcome");
+hash.Add("TTYL", "talk to you later");
+
+string input = "";
+do{
+    input = sr.ReadLine();
+    if(hash.ContainsKey(input)){
+        Console.WriteLine(hash[input]);
+    }else{
+        Console.WriteLine(input);
+    }
+}while(input != "TTYL");
+
+
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
---
+++ bronze4/6887.Program.cs
@@ -0,0 +1,21 @@
+﻿//https://www.acmicpc.net/problem/6887
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int squares = int.Parse(sr.ReadLine());
+Console.WriteLine("The largest square has side length {0}.", Math.Truncate(Math.Sqrt(squares)));
+
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
