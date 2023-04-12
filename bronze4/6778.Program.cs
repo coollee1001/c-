99fade263eb2496e47e16e781499a222992cb856
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-12 09:29:02 +0900

    40

---
+++ bronze4/6778.Program.cs
@@ -0,0 +1,24 @@
+﻿//https://www.acmicpc.net/problem/6778
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int antenna = int.Parse(sr.ReadLine());
+int eyes = int.Parse(sr.ReadLine());
+
+if(3 <= antenna && eyes <= 4) Console.WriteLine("TroyMartian");
+if(antenna <= 6 && 2 <= eyes) Console.WriteLine("VladSaturnian");
+if(antenna <= 2 && eyes <= 3) Console.WriteLine("GraemeMercurian");
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
---
+++ bronze4/6810.Program.cs
@@ -0,0 +1,27 @@
+﻿//https://www.acmicpc.net/problem/6810
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int sum = 91;
+for(int i=1; i<=3; i++){
+    if(i%2 == 0){
+        sum += int.Parse(sr.ReadLine()) * 3;
+    }else{
+        sum += int.Parse(sr.ReadLine());
+    }
+}
+Console.WriteLine("The 1-3-sum is {0}", sum);
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
