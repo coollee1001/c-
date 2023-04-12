cb85da2b4b4a5b614bf003d851267de00eafa310
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-13 04:00:07 +0900

    42

---
+++ bronze4/6888.Program.cs
@@ -0,0 +1,25 @@
+﻿//https://www.acmicpc.net/problem/6888
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int x = int.Parse(sr.ReadLine());
+int y = int.Parse(sr.ReadLine());
+
+for(int i=x; i<=y; i=i+60){
+    Console.WriteLine("All positions change in year {0}", i);
+}
+
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
---
+++ bronze4/6916.Program.cs
@@ -0,0 +1,32 @@
+﻿//https://www.acmicpc.net/problem/6916
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+string[] list = {
+    " * * *\n*     *\n*     *\n*     *\n\n*     *\n*     *\n*     *\n * * *",
+    "\n      *\n      *\n      *\n\n      *\n      *\n      *",
+    " * * *\n      *\n      *\n      *\n * * *\n*\n*\n*\n * * * ",
+    " * * *\n      *\n      *\n      *\n * * *\n      *\n      *\n      *\n * * *",
+    "\n*     *\n*     *\n*     *\n * * *\n      *\n      *\n      *\n",
+    " * * *\n*\n*\n*\n * * *\n      *\n      *\n      *\n * * *",
+    " * * *\n*\n*\n*\n * * *\n*     *\n*     *\n*     *\n * * *",
+    " * * *\n      *\n      *\n      *\n\n      *\n      *\n      *",
+    " * * *\n*     *\n*     *\n*     *\n * * *\n*     *\n*     *\n*     *\n * * *",
+    " * * *\n*     *\n*     *\n*     *\n * * *\n      *\n      *\n      *\n * * *"};
+int n = int.Parse(sr.ReadLine());
+Console.WriteLine(list[n]);
+
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
