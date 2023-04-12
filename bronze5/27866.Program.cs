da68923132f6073ee7c434e4fb20810613b46490
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-13 04:48:14 +0900

    43

---
+++ bronze5/27866.Program.cs
@@ -0,0 +1,22 @@
+﻿//https://www.acmicpc.net/problem/27866
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+string str = sr.ReadLine();
+int n = int.Parse(sr.ReadLine());
+Console.WriteLine(str[n-1]);
+
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
---
+++ bronze5/27889.Program.cs
@@ -0,0 +1,28 @@
+﻿//https://www.acmicpc.net/problem/27889
+
+using System.Collections;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+Hashtable ht = new Hashtable();
+ht.Add("NLCS", "North London Collegiate School");
+ht.Add("BHA", "Branksome Hall Asia");
+ht.Add("KIS", "Korea International School");
+ht.Add("SJA", "St. Johnsbury Academy");
+
+Console.WriteLine(ht[sr.ReadLine()]);
+
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
