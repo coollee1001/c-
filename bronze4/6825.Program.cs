e0e6b9cee5fbbdb7144f4ef1fbb360bbba2480e7
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-12 10:34:44 +0900

    check4

---
+++ bronze4/6825.Program.cs
@@ -0,0 +1,28 @@
+﻿//https://www.acmicpc.net/problem/6825
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+float weight = float.Parse(sr.ReadLine());
+float height = float.Parse(sr.ReadLine());
+float bmi = weight/(height * height);
+
+if(bmi < 18.5){
+    Console.WriteLine("Underweight");
+}else if(bmi <= 25){
+    Console.WriteLine("Normal weight");
+}else{
+    Console.WriteLine("Overweight");
+}
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
