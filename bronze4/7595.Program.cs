46a1f863fcb0a3d2d445c59e78eef041811c9028
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-13 09:03:09 +0900

    44

---
+++ bronze4/7595.Program.cs
@@ -0,0 +1,34 @@
+﻿//https://www.acmicpc.net/problem/7595
+
+using System.Text;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StringBuilder sb = new StringBuilder();
+sw.AutoFlush = true;
+
+string input = "";
+while((input = sr.ReadLine()) != "0"){
+    for(int i=0; i<int.Parse(input); i++){
+        for(int j=0; j<=i; j++){
+            sb.Append("*");
+        }
+        sb.Append("\n");
+    }
+}
+sw.WriteLine(sb.ToString());
+
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
---
+++ bronze4/8558.Program.cs
@@ -0,0 +1,25 @@
+﻿//https://www.acmicpc.net/problem/8558
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int n = int.Parse(sr.ReadLine());
+long sum = 1;
+for(int i=1; i<=n; i++){
+    sum *= i;
+}
+string strSum = sum.ToString();
+Console.WriteLine(strSum[strSum.Length-1]);
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
