c75899344ed192cab044a4ab5385e194b0726e09
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-13 11:00:22 +0900

    45

---
+++ bronze4/8674.Program.cs
@@ -0,0 +1,29 @@
+﻿//https://www.acmicpc.net/problem/8674
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+string[] inputdata = sr.ReadLine().Split(" ");
+long a = long.Parse(inputdata[0]);
+long b = long.Parse(inputdata[1]);
+if(a%2 == 0 && b%2 == 0){
+    Console.WriteLine(0);
+}else{
+    Console.WriteLine("{0}", Math.Min(a, b));
+}
+
+
+
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
---
+++ bronze4/8710.Program.cs
@@ -0,0 +1,28 @@
+﻿//https://www.acmicpc.net/problem/8710
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+string[] inputdata = sr.ReadLine().Split(" ");
+long k = long.Parse(inputdata[0]);
+long w = long.Parse(inputdata[1]);
+long m = long.Parse(inputdata[2]);
+int count = 0;
+while(k < w){
+    k += m;
+    count++;
+}
+Console.WriteLine(count);
+
+// github용 파일 복사
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Code Num > ");
+Console.ForegroundColor = ConsoleColor.White;
+Filecopy fc = new Filecopy(int.Parse(sr.ReadLine()));
+try{
+    File.Copy(fc.getThisPath(), fc.getTargetPath(), true);
+}catch(Exception e){
+    Console.WriteLine(e.ToString());
+}finally{
+    Console.WriteLine("save done");
+}
+
