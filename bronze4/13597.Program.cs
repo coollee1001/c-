bba1817af13e38feba8d7c77fbecb0a29e00e130
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-18 07:11:10 +0900

    1

---
+++ bronze4/13597.Program.cs
@@ -0,0 +1,15 @@
+﻿//https://www.acmicpc.net/problem/13597
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+var n = sr.ReadLine().Split(" ");
+var nInt = new int[n.Length];
+for(int i=0; i<n.Length; i++){
+    nInt[i] = int.Parse(n[i]);
+}
+Console.WriteLine(Math.Max(nInt[0], nInt[1]));
+
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
---
+++ bronze4/13623.Program.cs
@@ -0,0 +1,26 @@
+﻿//https://www.acmicpc.net/problem/13623
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+var n = sr.ReadLine().Split(" ");
+var nInt = new int[n.Length];
+var sum = 0;
+string[] result = {"*", "A", "B", "C"};
+for(int i=0; i<n.Length; i++){
+    nInt[i] = int.Parse(n[i]);
+    sum += nInt[i];
+}
+for(int i=0; i<n.Length; i++){
+    if(sum == 1 && nInt[i] == 1) {
+        Console.Write(result[i+1]);
+        return;
+    } else if(sum == 2 && nInt[i] == 0) {
+        Console.Write(result[i+1]);
+        return;
+    }
+}
+Console.WriteLine(result[0]);
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
