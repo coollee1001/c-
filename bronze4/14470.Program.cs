4924341330fbac934276a6a1588f7ca861cbed04
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-19 07:00:29 +0900

    5

---
+++ bronze4/14470.Program.cs
@@ -0,0 +1,21 @@
+﻿//https://www.acmicpc.net/problem/14470
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+int[] inputData = new int[5];
+for(int i=0; i<inputData.Length; i++){
+    inputData[i] = int.Parse(sr.ReadLine());
+}
+int second = 0;
+if(inputData[0] < 0) {
+    second += Math.Abs(inputData[0]) * inputData[2];
+    second += inputData[3];
+    second += inputData[1] * inputData[4];
+}else{
+    second = (inputData[1] - inputData[0]) * inputData[4];
+}
+
+Console.WriteLine(second);
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
---
+++ bronze4/14489.Program.cs
@@ -0,0 +1,19 @@
+﻿//https://www.acmicpc.net/problem/14489
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+var account = sr.ReadLine().Split(" ");
+var accountArr = Array.ConvertAll(account, int.Parse);
+
+var chicken = int.Parse(sr.ReadLine());
+var accountSum = accountArr[0] + accountArr[1];
+if(accountSum < chicken * 2){
+    Console.WriteLine(accountSum);
+}else{
+    Console.WriteLine(accountSum - (chicken * 2));
+}
+
+
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
