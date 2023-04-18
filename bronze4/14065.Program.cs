2334e8dd39c15b13ad53b69dcf881718b83da1d3
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-18 15:05:18 +0900

    4

---
+++ bronze4/14065.Program.cs
@@ -0,0 +1,12 @@
+﻿//https://www.acmicpc.net/problem/14065
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+double input = double.Parse(sr.ReadLine());
+double galon = 3.785411784d;
+double milja = 1.609344d;
+
+Console.WriteLine("{0:0.000000}", 100.0 / (input*(milja/galon)));
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
---
+++ bronze4/14173.Program.cs
@@ -0,0 +1,28 @@
+﻿//https://www.acmicpc.net/problem/14173
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+var first = sr.ReadLine().Split(" ");
+var second = sr.ReadLine().Split(" ");
+
+int[] min = {99, 99};
+int[] max = {0, 0};
+
+for(int i=0; i<first.Length; i+=2){
+    if(int.Parse(first[i]) < min[0]) min[0] = int.Parse(first[i]);
+    if(int.Parse(first[i+1]) < min[1]) min[1] = int.Parse(first[i+1]);
+    if(max[0] < int.Parse(first[i])) max[0] = int.Parse(first[i]);
+    if(max[1] < int.Parse(first[i+1])) max[1] = int.Parse(first[i+1]);
+    
+    if(int.Parse(second[i]) < min[0]) min[0] = int.Parse(second[i]);
+    if(int.Parse(second[i+1]) < min[1]) min[1] = int.Parse(second[i+1]);
+    if(max[0] < int.Parse(second[i])) max[0] = int.Parse(second[i]);
+    if(max[1] < int.Parse(second[i+1])) max[1] = int.Parse(second[i+1]);
+}
+
+int length = Math.Max(max[0]-min[0], max[1]-min[1]);
+Console.WriteLine("{0}", length * length);
+
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
