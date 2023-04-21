4b0aef9ce488f530c2b0e27bd72dd1f9efed8438
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-04-22 08:27:59 +0900

    30

---
+++ bronze4/16727.Program.cs
@@ -0,0 +1,29 @@
+﻿// https://www.acmicpc.net/problem/16727
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+var a = sr.ReadLine().Split(" ");
+var b = sr.ReadLine().Split(" ");
+
+var aArr = Array.ConvertAll(a, double.Parse);
+var bArr = Array.ConvertAll(b, double.Parse);
+
+var aSum = aArr[0] + bArr[1];
+var bSum = aArr[1] + bArr[0];
+
+if(aSum == bSum){
+    if(aArr[0] == bArr[0]) Console.WriteLine("Penalty");
+    else Console.WriteLine(aArr[0]<bArr[0]?"Persepolis":"Esteghlal");
+}else{
+    Console.WriteLine(aSum<bSum?"Esteghlal":"Persepolis");
+}
+
+
+
+
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
+
+
---
+++ bronze4/17009.Program.cs
@@ -0,0 +1,21 @@
+﻿// https://www.acmicpc.net/problem/17009
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+const int MAX = 3;
+int a = 0, b = 0;
+for(int i=0; i<MAX; i++)
+    a += int.Parse(sr.ReadLine()) * (MAX - i);
+for(int i=0; i<MAX; i++)
+    b += int.Parse(sr.ReadLine()) * (MAX - i);
+Console.WriteLine(a<b?"B":a==b?"T":"A");
+
+
+
+
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
+
+
