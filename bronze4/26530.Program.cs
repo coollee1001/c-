1bade87fe6ff53cbf14ac019b81a99bf28800056
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-05-03 20:19:18 +0900

    21

---
+++ bronze4/26530.Program.cs
@@ -0,0 +1,18 @@
+﻿// https://www.acmicpc.net/problem/26530
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int n = int.Parse(sr.ReadLine());
+while(n-- != 0){
+    int m = int.Parse(sr.ReadLine());
+    double sum = 0.0d;
+    while(m-- != 0){
+        var data = sr.ReadLine().Split(" ");
+        sum += double.Parse(data[1]) * double.Parse(data[2]);
+    }
+    Console.WriteLine("${0:0.00}", sum);
+}
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
---
+++ bronze5/2393.Program.cs
@@ -0,0 +1,22 @@
+﻿// https://www.acmicpc.net/problem/2393
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+Console.WriteLine("  ___  ___  ___");
+Console.WriteLine("  | |__| |__| |");
+Console.WriteLine("  |           |");
+Console.WriteLine("   \\_________/");
+Console.WriteLine("    \\_______/");
+Console.WriteLine("     |     |");
+Console.WriteLine("     |     |");
+Console.WriteLine("     |     |");
+Console.WriteLine("     |     |");
+Console.WriteLine("     |_____|");
+Console.WriteLine("  __/       \\__");
+Console.WriteLine(" /             \\");
+Console.WriteLine("/_______________\\");
+
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
