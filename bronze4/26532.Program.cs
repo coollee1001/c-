f3a3d5876c163681b21192b0f5fcd977a2ba02c6
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-05-04 20:03:25 +0900

    23

---
+++ bronze4/26532.Program.cs
@@ -0,0 +1,15 @@
+﻿// https://www.acmicpc.net/problem/26532
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+var data = Array.ConvertAll(sr.ReadLine().Split(" "), double.Parse);
+double yard = 4840.0;
+double corn = 5.0;
+
+int bag = (int)Math.Ceiling(((double)(data[0]*data[1])/yard)/corn);
+Console.WriteLine(bag);
+
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
---
+++ bronze4/26546.Program.cs
@@ -0,0 +1,15 @@
+﻿// https://www.acmicpc.net/problem/26546
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int n = int.Parse(sr.ReadLine());
+while(n-- != 0){
+    var data = sr.ReadLine().Split(" ");
+    Console.WriteLine(data[0].Substring(0, int.Parse(data[1])) + data[0].Substring(int.Parse(data[2])));
+}
+
+
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
