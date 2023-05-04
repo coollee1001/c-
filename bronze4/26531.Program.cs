ead7bed1e1233ff2546be00e53eed57f49a5a7fe
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-05-04 18:27:20 +0900

    22

---
+++ bronze4/26531.Program.cs
@@ -0,0 +1,12 @@
+﻿// https://www.acmicpc.net/problem/26531
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+var data = sr.ReadLine().Split(" ");
+string result = "NO";
+if(int.Parse(data[0]) + int.Parse(data[2]) == int.Parse(data[4])) result = "YES";
+Console.WriteLine(result);
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
