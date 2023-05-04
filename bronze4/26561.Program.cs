1d9ed8182c21c0718dc3983c412bba7fa73bcbaf
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-05-04 22:02:18 +0900

    25

---
+++ bronze4/26561.Program.cs
@@ -0,0 +1,15 @@
+﻿// https://www.acmicpc.net/problem/26561
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int n = int.Parse(sr.ReadLine());
+while(n-- != 0){
+    var data = Array.ConvertAll(sr.ReadLine().Split(" "), int.Parse);
+    data[0] -= data[1]/7;
+    data[0] += data[1]/4;
+    Console.WriteLine(data[0]);
+}
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
