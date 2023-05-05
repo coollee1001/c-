455fff8a7d5ddd1b6c1e5326dc636bd59275ab74
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-05-05 15:39:36 +0900

    27

---
+++ bronze4/26566.Program.cs
@@ -0,0 +1,16 @@
+﻿// https://www.acmicpc.net/problem/26566
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int n = int.Parse(sr.ReadLine());
+while(n-- != 0){
+    var data1 = Array.ConvertAll(sr.ReadLine().Split(" "), double.Parse);
+    var data2 = Array.ConvertAll(sr.ReadLine().Split(" "), double.Parse);
+    double sliced = (double)data1[0]/data1[1];
+    double whole = (double)(Math.PI * Math.Pow(data2[0], 2))/data2[1];
+    Console.WriteLine(sliced>whole?"Slice of pizza":"Whole pizza");
+}
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
---
+++ bronze4/26592.Program.cs
@@ -0,0 +1,13 @@
+﻿// https://www.acmicpc.net/problem/26592
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int n = int.Parse(sr.ReadLine());
+while(n-- != 0){
+    var data = Array.ConvertAll(sr.ReadLine().Split(" "), double.Parse);
+    Console.WriteLine("The height of the triangle is {0:0.00} units", (data[0]*2)/data[1]);
+}
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
