400367a81c27e202a35e6801a1ef13da43e4708a
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-06-11 17:14:40 +0900

    ...

---
+++ bronze3/4153.Program.cs
@@ -0,0 +1,29 @@
+﻿// https://www.acmicpc.net/problem/4153
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+while(true){
+    var data = Array.ConvertAll(sr.ReadLine().Split(" "), int.Parse);
+    
+    // 종료
+    if(data[0] == 0 && data[1] == 0 && data[2] == 0) {
+        break;
+    }
+
+    // 정렬
+    Array.Sort(data);
+
+    // 직각삼각형 판단
+    if(Math.Pow(data[0], 2) + Math.Pow(data[1], 2) == Math.Pow(data[2], 2)){
+        sw.WriteLine("right");
+    }else{
+        sw.WriteLine("wrong");
+    }
+}
+
+sw.Close();
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
