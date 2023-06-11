626dda5051057a60180ff6b861d5516fdf98b509
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-06-11 17:57:53 +0900

    .

---
+++ bronze3/4435.Program.cs
@@ -0,0 +1,49 @@
+﻿// https://www.acmicpc.net/problem/4435
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+// 각 종족의 점수 배열
+int[] gandalfArray = {1,2,3,3,4,10};
+int[] sauronArray = {1,2,2,2,3,5,10};
+
+// 전투 개수
+int n = int.Parse(sr.ReadLine());
+for(int i=1; i<=n; i++){
+
+    // 데이터 입력
+    var gandalf = Array.ConvertAll(sr.ReadLine().Split(" "), int.Parse);
+    var sauron = Array.ConvertAll(sr.ReadLine().Split(" "), int.Parse);
+
+    // 초기화
+    int gsum = 0;
+    int ssum = 0;
+    string result;
+
+    // 간달프 군대 점수
+    for(int j=0; j<gandalf.Length; j++){
+        gsum += gandalf[j] * gandalfArray[j];
+    }
+
+    // 사우론 군대 점수
+    for(int j=0; j<sauron.Length; j++){
+        ssum += sauron[j] * sauronArray[j];
+    }
+
+    // 어느 쪽이 이기는지 판단
+    if(gsum == ssum){
+        result = "No victor on this battle field";
+    }else if(gsum < ssum){
+        result = "Evil eradicates all trace of Good";
+    }else{
+        result  = "Good triumphs over Evil";
+    }
+
+    // 결과 출력
+    sw.WriteLine(String.Format("Battle {0}: {1}", i, result));
+}
+sw.Close();
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
---
+++ bronze3/4458.Program.cs
@@ -0,0 +1,20 @@
+﻿// https://www.acmicpc.net/problem/4458
+
+using System.Text;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int n = int.Parse(sr.ReadLine());
+
+while(n-- != 0){
+    var data = sr.ReadLine();
+    StringBuilder sb = new StringBuilder(data);
+    sb[0] = char.Parse(data.Substring(0,1).ToUpper());
+    sw.WriteLine(sb);
+}
+sw.Close();
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
