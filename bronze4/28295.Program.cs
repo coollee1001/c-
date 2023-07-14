2089cc70a191fdb5f51db1e22993975086265064
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-07-14 21:18:07 +0900

    230714

---
+++ bronze4/28295.Program.cs
@@ -0,0 +1,18 @@
+﻿// https://www.acmicpc.net/problem/28295
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int idx = 0;
+string s = "NESW";
+for(int i=0; i<10; i++){
+    int data = int.Parse(sr.ReadLine());
+    idx += data;
+}
+idx %= 4;
+sw.WriteLine(s[idx]);
+sw.Close();
+
+// github 업로드용 파일 백업
+Filecopy fc = new Filecopy();
+fc.copyLogic();
